d9da4592a28e87db64dffcf550f379b3260d45b4
Jibran Syed <jisyed14@gmail.com>
2014-04-12 21:06:49 -0500

    Started implementing Timer class. Not done yet.

---
+++ SF3DEW/SF3DEW/Timer.cpp
@@ -0,0 +1,129 @@
+#include "Timer.hpp"
+
+#include <iostream>
+
+namespace sfew
+{
+	// Ctor/Dtor ========================================
+
+	// Ctor
+	Timer::Timer(sf::Time setTime, Callback function) :
+		_triggerTime(setTime),
+		_triggerEvent(function),
+		_loops(false),
+		_alreadyTriggered(false)
+	{
+		
+	}
+
+	// Dtor
+	Timer::~Timer()
+	{
+		
+	}
+
+	// Routines =========================================
+
+	// Update timer in game loop
+	void Timer::Update()
+	{
+
+	}
+
+	// Adds time to make timer trigger later
+	void Timer::AddTime(float addedSeconds)
+	{
+		sf::Time addedTime(sf::seconds(addedSeconds));
+		_triggerTime += addedTime;
+	}
+
+	// Adds time to make timer trigger later
+	void Timer::AddTime(sf::Time addedTime)
+	{
+		_triggerTime += addedTime;
+	}
+
+	// Resets the clock to countdown the trigger
+	void Timer::Reset()
+	{
+		_alreadyTriggered = false;
+		_countdown.restart();
+	}
+
+	// Properties =========================================
+
+	void Timer::SetCallback(Callback newCallback)
+	{
+		_triggerEvent = newCallback;
+	}
+
+	void Timer::SetLooping(bool loop)
+	{
+		if(loop == true)
+		{
+			_alreadyTriggered = false;
+		}
+
+		_loops = loop;
+	}
+
+	bool Timer::IsLooping() const
+	{
+		return _loops;
+	}
+
+	void Timer::SetTime(sf::Time newTime)
+	{
+		_triggerTime = newTime;
+	}
+
+	void Timer::SetTime(float newSeconds)
+	{
+		sf::Time newTime(sf::seconds(newSeconds));
+		_triggerTime = newTime;
+	}
+
+	sf::Time Timer::GetTime() const
+	{
+		return _triggerTime;
+	}
+
+	bool Timer::AlreadyTriggered() const
+	{
+		return _alreadyTriggered;
+	}
+
+	sf::Time Timer::GetTimeElapsed() const
+	{
+		return _countdown.getElapsedTime();
+	}
+
+	sf::Time Timer::GetTimeRemaining() const
+	{
+		sf::Time elapsed = _countdown.getElapsedTime();
+		sf::Time remaining = _triggerTime - elapsed;
+		if(remaining.asSeconds() < 0.0f)
+		{
+			remaining = sf::Time::Zero;
+		}
+
+		return remaining;
+	}
+
+	// Helpers =========================================
+
+	// Fire the triggered event
+	void Timer::executeCallback()
+	{
+		// Check if target is valid
+		if(_triggerEvent.target<Callback>() == nullptr)
+		{
+			std::cout << "Error! Timer's callback has no target!" << std::endl;
+			return;
+		}
+
+		// Fire the callback
+		_triggerEvent();
+	}
+
+} // namespace sfew
---
+++ SF3DEW/SF3DEW/Timer.hpp
@@ -0,0 +1,67 @@
+// Timer
+// Counts down a certain time and then 
+// triggers a callback from the timer's delegate object
+// Setting callbacks must be done with std::bind()
+//
+// Written by Jibran Syed (4-12-2014)
+
+#ifndef SFEW_TIMER_H
+#define SFEW_TIMER_H
+
+#include <functional>
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Time.hpp>
+#include <SFML/System/NonCopyable.hpp>
+
+namespace sfew
+{
+	typedef std::function<void()> Callback;
+
+	class Timer : sf::NonCopyable
+	{
+	public:
+
+		// Ctor/Dtor ======================
+
+		Timer(sf::Time setTime, Callback function);
+		~Timer();
+
+		// Routines =======================
+
+		void Update();						// Update timer in game loop
+		void AddTime(float addedSeconds);	// Adds time to make timer trigger later
+		void AddTime(sf::Time addedTime);	// Adds time to make timer trigger later
+		void Reset();						// Resets the clock to countdown the trigger
+
+		// Properties =====================
+
+		void SetCallback(Callback newCallback);
+		void SetLooping(bool loop);
+		bool IsLooping() const;
+		void SetTime(sf::Time newTime);
+		void SetTime(float newSeconds);
+		sf::Time GetTime() const;
+		bool AlreadyTriggered() const;
+		sf::Time GetTimeElapsed() const;
+		sf::Time GetTimeRemaining() const;
+
+	private:
+
+		// Helpers ========================
+
+		void executeCallback();		// Fire the triggered event
+
+		// Data ===========================
+
+		bool _loops;				// Does the timer trigger in repeated intervals?
+		sf::Clock _countdown;		// Increments time until the trigger time
+		sf::Time _triggerTime;		// Total amount of time to elapse since creation
+		Callback _triggerEvent;		// Function to call when timer has triggered
+		bool _alreadyTriggered;		// Did the timer already trigger?
+
+	};
+
+	
+} // namespace sfew
+
+#endif
