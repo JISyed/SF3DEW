7378a88ded0273b8c7020af490d7c35560482a19
Jibran Syed <jisyed14@gmail.com>
2014-04-18 21:41:29 -0500

    Adding a lot of empty code files

---
+++ SF3DEW/SF3DEW/TimerContainer.cpp
@@ -0,0 +1,25 @@
+#include "TimerContainer.hpp"
+
+#include <iostream>
+
+namespace sfew
+{
+	// Ctor/Dtor ========================================
+
+	TimerContainer::TimerContainer()
+	{
+		
+	}
+
+	TimerContainer::~TimerContainer()
+	{
+		
+	}
+
+	// Routines =========================================
+
+	// Properties =========================================
+
+	// Helpers =========================================
+
+} // namespace sfew
---
+++ SF3DEW/SF3DEW/TimerContainer.hpp
@@ -0,0 +1,45 @@
+// TimerContainer
+// Container to update Timer objects
+//
+// Written by Jibran Syed (4-18-2013)
+
+#ifndef SFEW_TIMER_CONTAINER_H
+#define SFEW_TIMER_CONTAINER_H
+
+// SYSTEM HEADERS HERE
+
+#include "Container.hpp"
+
+namespace sfew
+{
+	class TimerContainer
+	{
+	public:
+
+		// Ctor/Dtor ======================
+
+		TimerContainer();
+		~TimerContainer();
+
+		// Routines =======================
+
+		
+
+		// Properties =====================
+
+	private:
+
+		// Helpers ========================
+
+		
+
+		// Data ===========================
+
+		
+		
+	};
+
+	
+} // namespace sfew
+
+#endif
